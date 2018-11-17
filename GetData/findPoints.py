ba82e7c81486f8cb03dfcbedc8640a327ff11cfb
mohitrajane <mhit98@gmail.com>
2018-11-17 00:32:05 +0000

    implement funcation to send angle and distance from arduino to python. Also tidy the pyhton file by adding functions

--- GetData/findPoints.py
+++ GetData/findPoints.py
@@ -1,43 +1,84 @@
-import serial
-import numpy
+import serial,numpy,time
+import serial.tools.list_ports
 
-port = input("Enter port : ")
+def get_arduino_port():
+    ports_list = list(serial.tools.list_ports.comports())
+    for p in ports_list:
+        device = str(p.description)
+        if "ACM" in device:
+            p1 = "/dev/" + device
+            return p1
+        else:
+            print("Arduino not found")
+        
+        
 
-ser = serial.Serial('/dev/ttyACM0',9600)
+def find_position(distance,phi,theta):
+    x =  (link1_length + distance) * numpy.cos(theta) * numpy.sin(phi)
+    y = base_link_length + distanceFromFloor + (link1_length + distance) * numpy.sin(theta)
+    z = (link1_length + distance) * numpy.cos(theta) * numpy.cos(phi)
+    return x,y,z
 
-l = 10
-r = 10
-BaseTurns = 86
-LinkTurns = BaseTurns
-singlePhi = (360/BaseTurns)
-singleTheta = (180/LinkTurns)
-distanceFromFloor = 0
-noOfPoints = BaseTurns*LinkTurns
-phi = 0
-theta = 0
 
-f = open("points.pcd","w")
+def data_parser(raw_data):
+    parameters = raw_data.split(',')
+    dist = float(parameters[0])
+    phi = float(parameters[1])
+    theta = float(parameters[2])
+    #print("distance:" + str(dist) + " phi:" + str(phi) + " theta:" + str(theta))
+    return dist,phi,theta
+    
 
-f.write("# .PCD v0.7 - Point Cloud Data file format\n")
-f.write("VERSION 0.7\n")
-f.write("FIELDS x y z\n")
-f.write("SIZE 4 4 4\n")
-f.write("TYPE F F F\n")
-f.write("COUNT 1 1 1\n")
-f.write("WIDTH "+ str(noOfPoints) +"\n")
-f.write("HEIGHT 1\n")
-f.write("VIEWPOINT 0 0 0 1 0 0 0\n")
-f.write("POINTS "+ str(noOfPoints) +"\n")
-f.write("DATA ascii\n")
 
-for i in range (0, BaseTurns) :
-    for j in range (0, LinkTurns) :
-        phi += singlePhi
-        theta += singleTheta
-        distance = ser.read(4)      #Size of dimension (of data received) is 4 bytes
-        x = (r+distance)*numpy.cos(theta)*numpy.sin(phi)
-        y = l + distanceFromFloor + (r+distance)*numpy.sin(theta)
-        z = (r+distance)*numpy.cos(theta)*numpy.cos(phi)
-        f.write(str(x)+" "+str(y)+" "+str(z)+"\n")
+if __name__ == "__main__":
 
-f.close()
+    #length of links in cm
+    base_link_length = 20 
+    link1_length = 19.5
+    BaseTurns = 7 #10000 
+    LinkTurns = 38 #10000 
+    singlePhi = (360/BaseTurns)
+    singleTheta = 10
+    distanceFromFloor = 0
+    noOfPoints = BaseTurns * LinkTurns
+
+    port = get_arduino_port()
+    print("Arduino found in " + port)
+    print("connecting")
+    ser = serial.Serial(port,9600)
+
+
+    #open file and initilize pcd values
+    f = open("points.pcd","w")
+    f.write("# .PCD v0.7 - Point Cloud Data file format\n")
+    f.write("VERSION 0.7\n")
+    f.write("FIELDS x y z\n")
+    f.write("SIZE 4 4 4\n")
+    f.write("TYPE F F F\n")
+    f.write("COUNT 1 1 1\n")
+    f.write("WIDTH "+ str(noOfPoints) +"\n")
+    f.write("HEIGHT 1\n")
+    f.write("VIEWPOINT 0 0 0 1 0 0 0\n")
+    f.write("POINTS "+ str(noOfPoints) +"\n")
+    f.write("DATA ascii\n")
+
+
+    for i in range (0, BaseTurns):
+        for j in range (0, LinkTurns):
+            #phi += singlePhi
+            #theta += singleTheta
+            time.sleep(1)
+            ser.flushInput()
+            #while ser.in_waiting :
+            raw_distance = ser.readline()
+            distance,phi,theta = data_parser(raw_distance.decode())
+            #distance = float(raw_distance.decode())
+            #print(distance)
+            x,y,z = find_position(distance,phi,theta)
+            print("phi: " + str(phi) + " theta: " + str(theta))
+            print("x:" + str(x) + " y:" + str(y) + " z:" + str(z) + "\n")
+            f.write(str(x)+" "+str(y)+" "+str(z)+"\n")
+            print("i:" + str(i) + "j:" + str(j))
+
+    print("PCD file generated")
+    f.close()
--- SweepBoth/SweepWithSensor/SweepWithSensor.ino
+++ SweepBoth/SweepWithSensor/SweepWithSensor.ino
@@ -7,65 +7,70 @@ Servo servo_1;
 #define TURN_TIME 185     //Variation from calculated value : Trial and error of time_delay
 //turn_time = (0.23 - 0.19) *(5-4.8)/(4.8 - 6.0) + 0.23 = 0.22333sec 0.23 is speed at 4.8V and 0.19 is speed at 6.0V
 
+
+// defines pins numbers
+const int trigPin = 5;
+const int echoPin = 6;
+const int base_pin = 9;
+const int servo1_pin = 10;
+
+
+// defines variables
+long duration;
+float distance;
+String servo1_angle_str,servo_base_str;
 int time_delay;
 int angle = 5;
 int finalAngle =5;
 int servo1_angle=10;
 
-// defines pins numbers
-const int trigPin = 5;
-const int echoPin = 6;
-
-// defines variables
-long duration;
-float distance;
 
 void setup() {
   
-  servo_base.attach(9);
-  servo_1.attach(10);
-  
+  servo_base.attach(base_pin);
+  servo_1.attach(servo1_pin);
   servo_1.write(0);
-  
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);
-  
   time_delay = (TURN_TIME *(angle*0.01746031746));  // 0.017.. is 22/(7*180) converting phi to rad and multiplying with turn time
 }
 
 void loop() {
   
-      //Move Clockwise
       while (finalAngle <= 35) {    //this makes it to rotate 360
         servo_base.writeMicroseconds(1000);
         delay(200);
         //servo_base.writeMicroseconds(1500);
         servo_base.write(90);
         delay(500);
-        //findDistance();
+
         for(servo1_angle = 0; servo1_angle <= 180; servo1_angle = servo1_angle+10){
           servo_1.write(servo1_angle);
-          delay(200);
-          //findDistance();
+          delay(1000);
+          servo1_angle_str = String(servo1_angle);
+          servo_base_str = String(finalAngle);
+          findDistance();
         }
-        
         delay(2000);
-        //findDistance();
+    
         for(servo1_angle = 180; servo1_angle >= 0; servo1_angle = servo1_angle-10){
           servo_1.write(servo1_angle);
-          delay(200);
-          //findDistance();
+          delay(1000);
+          servo1_angle_str = String(servo1_angle);
+          servo_base_str = String(finalAngle);
+         findDistance();
         }
         delay(1000);
         //delay(time_delay-75);  //8
        
         //findDistance();
         finalAngle = finalAngle + 5;
-        Serial.println(finalAngle);
+        //Serial.println(finalAngle);
         delay(1000);
-      }
+      }    
 }
+
 
 void findDistance(){
     // Clears the trigPin
@@ -79,16 +84,12 @@ void findDistance(){
     
     // Reads the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
-
-    Serial.print("\nDuration: ");
-    Serial.println(duration);
     
-    // Calculating the distance
+    // Calculating the distanceMM
     //Speed of sound = 340 m/s = 0.034cm/microsecond
     //Distance = (Speed * Time)/2
     distance= duration*0.034/2;
     
-    // Prints the distance on the Serial Monitor
-    Serial.print("Distance: ");
-    Serial.println(distance);
+    String distance_string = String(distance);
+    Serial.println(distance_string + "," + servo_base_str + "," +servo1_angle_str);
 }
